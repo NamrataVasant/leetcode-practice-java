7398ccc193c38d0f305e14e2bd21074b2b905a57
NamrataVasant <namratanaik1709@gmail.com>
2022-06-12 15:45:42 -0400

    Concatenation of Array and Defanging an IP Address problems

--- src/easy/ConcatenationOfArray.java
+++ src/easy/ConcatenationOfArray.java
@@ -1,2 +1,23 @@
-package easy;public class ConcatenationOfArray {
+package easy;
+
+public class ConcatenationOfArray {
+
+    private static int[] getConcatenation(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[2 * n];
+        for (int i = 0; i < n; i++) {
+            result[i] = nums[i];
+            result[i + n] = nums[i];
+        }
+        return result;
+    }
+
+    public static void main (String[] args) {
+        int[] nums = {1, 2, 1};
+        System.out.println("Result: ");
+        int[] answer = getConcatenation(nums);
+        for (int ans : answer) {
+            System.out.print(ans + ", ");
+        }
+    }
 }
--- src/easy/DefangingAnIpAddress.java
+++ src/easy/DefangingAnIpAddress.java
@@ -1,2 +1,21 @@
-package easy;public class DefangingAnIpAddress {
+package easy;
+
+public class DefangingAnIpAddress {
+
+    private static String defangIPaddr(String address) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < address.length(); i++) {
+            if (address.charAt(i) == '.') {
+                sb.append("[.]");
+            } else {
+                sb.append(address.charAt(i));
+            }
+        }
+        return sb.toString();
+    }
+
+    public static void main (String[] args) {
+        String ipAddress = "1.1.1.1";
+        System.out.println(defangIPaddr(ipAddress));
+    }
 }
